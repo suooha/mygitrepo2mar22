7cb560ff4607fb5acb9a45fc0a22b71490f4bca0
ooha <ooha@gmail.com>
2022-03-03 15:19:18 +0530

    my 100% completed

--- myjavaregpage.java
+++ myjavaregpage.java
@@ -1,2 +1,9 @@
 this is my java registration page
+<<<<<<< Updated upstream
 this is my manager work has completed
+=======
+1st day of java line of code
+2nd day of java line of code
+3rd day  of java line of code
+4th day of java line of code 90% of task completed
+5th day of java line of code
